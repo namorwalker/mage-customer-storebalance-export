fd12a28ff787a5d70921bb0ce0da1e7c65237f08
Namor Walker <namor.walker@victoriabeckham.com>
2016-04-20 11:19:24 +0100

    fixed setup resource method

--- app/code/local/Nwalkere/Balanceexport/Model/Resource/Setup.php
+++ app/code/local/Nwalkere/Balanceexport/Model/Resource/Setup.php
@@ -4,23 +4,35 @@ class Nwalkere_Balanceexport_Model_Resource_Setup extends Mage_Core_Model_Resour
 
     /**
      * This method returns true if the attribute exists.
-     * 
+     *
      * @param string|int $entityTypeId
      * @param string|int $attributeId
      * @return bool
      */
-    public function attributeExists($entityTypeId, $attributeId) 
-    {
-        try 
-        {
-            $entityTypeId = $this->getEntityTypeId($entityTypeId);
-            $attributeId = $this->getAttributeId($entityTypeId, $attributeId);
-            return !empty($attributeId);
-        } 
-        catch(Exception $e) 
-        {
-            return FALSE;
+    public function attributeValueExists($arg_attribute, $arg_value)	{
+
+        try{
+
+            $attribute_model = Mage::getModel('eav/entity_attribute');
+            $attribute_code = $attribute_model->getIdByCode($arg_value, $arg_attribute);
+            $attribute = $attribute_model->load($attribute_code);
+            $attribute_id = $attribute->getId();
+
+            if( $attribute_code && !empty($attribute_id) ){
+
+                return true;
+            }else{
+
+                return false;
+            }
+
+        }catch(Exception $e){
+
+            return false;
         }
+
+
+
     }
 
 }
