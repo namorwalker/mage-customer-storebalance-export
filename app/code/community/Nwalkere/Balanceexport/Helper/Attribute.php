9c3ca78f47e080679523a0e61f41285a3a92445b
Namor Walker <namor.walker@victoriabeckham.com>
2017-02-17 13:02:50 +0000

    fixed issues outlined in VB-1604

--- app/code/community/Nwalkere/Balanceexport/Helper/Attribute.php
+++ app/code/community/Nwalkere/Balanceexport/Helper/Attribute.php
@@ -30,12 +30,11 @@ class Nwalkere_Balanceexport_Helper_Attribute extends Mage_Core_Helper_Abstract
             return false;
         }
 
-
-
     }
 
+
     public function synchStoreBalanceAttribute(){
-        
+
         $attribute_exists = Mage::helper('balanceexport/attribute')->attributeExists("customer", "storebalanceexportcsvx");
 
         if( !$attribute_exists ){
@@ -43,35 +42,38 @@ class Nwalkere_Balanceexport_Helper_Attribute extends Mage_Core_Helper_Abstract
         }
 
         //update the new attribute with customer store balance
-        $customers = Mage::getModel('customer/customer')->getCollection();
+        $customers = Mage::getModel('customer/customer')
+            ->getCollection()
+            ->addAttributeToSelect('website_id')
+            ->addAttributeToSelect('storebalanceexportcsvx')
+            ->addAttributeToSelect('entity_id');
+
+        $customerBalancesCollection = Mage::getModel('enterprise_customerbalance/balance')
+            ->getCollection()
+            ->loadData();
+
+        $customerBalanceData = $customerBalancesCollection->getData();
+        $customerBalanceDataIndexed = array();
+
+        foreach ( $customerBalanceData as $key=>$val )
+        {
+            $customerBalanceDataIndexed[$customerBalanceData[$key]['customer_id']] = $customerBalanceData[$key];
+        }
+
+        $customerBalancesCollection = null;
 
         foreach($customers as $customer){
 
             $customerId = $customer->getId();
 
-            $currentCustomer = Mage::getModel('customer/customer')->load($customerId);
+            $existingBalance = $customerBalanceDataIndexed[$customerId]['amount'];
+            $csvxBalance = $customer->getStorebalanceexportcsvx();
 
-            $websiteId = $currentCustomer->getWebsiteId();
 
-            $balanceModel = Mage::getModel('enterprise_customerbalance/balance')
-                ->setCustomerId($customerId)
-                ->setWebsiteId($websiteId)
-                ->loadByCustomer();
-
-            // check if balance found
-            if (!$balanceModel->getId()) {
-                $balance = (float) 0;
-            }else{
-
-                $balance = $balanceModel->getAmount();
+            if( $existingBalance !=  $csvxBalance ||  !is_float($csvxBalance) ){
+                $customer->setStorebalanceexportcsvx($existingBalance);
+                $customer->getResource()->saveAttribute($customer,'storebalanceexportcsvx');
             }
-
-            $currentCustomer->setStorebalanceexportcsvx($balance);
-
-            //the original version of this answer was wrong; need to use the resource model.
-            $currentCustomer->getResource()->saveAttribute($currentCustomer,'storebalanceexportcsvx');
-
-            $currentCustomer->save();
 
         }
 
--- app/code/community/Nwalkere/Balanceexport/sql/nwalkere_balanceexport_setup/install-0.1.0.php
+++ app/code/community/Nwalkere/Balanceexport/sql/nwalkere_balanceexport_setup/install-0.1.0.php
@@ -19,7 +19,7 @@ if( !$attribute_exists ){
 	$attributeGroupId = $eavSetup->getDefaultAttributeGroupId($entityTypeId, $attributeSetId);
 
 
-	    $eavSetup->addAttribute("customer", "storebalanceexportcsvx",  array(
+	$eavSetup->addAttribute("customer", "storebalanceexportcsvx",  array(
 		"type"     => "varchar",
 		"backend"  => "",
 		"label"    => "Store Balance (export) d",
@@ -32,60 +32,25 @@ if( !$attribute_exists ){
 		"unique"     => false,
 		"note"       => "Store Balance (export) d"
 
-	    ));
+	));
 
 
-	    $attribute = Mage::getSingleton("eav/config")->getAttribute("customer", "storebalanceexportcsvx");
+	$attribute = Mage::getSingleton("eav/config")->getAttribute("customer", "storebalanceexportcsvx");
 
-	    $used_in_forms=array();
+	$used_in_forms=array();
 
 
-	    $attribute->setData("used_in_forms", $used_in_forms)
+	$attribute->setData("used_in_forms", $used_in_forms)
 		->setData("is_used_for_customer_segment", true)
 		->setData("is_system", 0)
 		->setData("is_user_defined", 1)
 		->setData("is_visible", 0)
 		->setData("sort_order", 100);
-	    $attribute->save();
+	$attribute->save();
 
 }
 
-
-
-//update the new attribute with customer store balance 
-$customers = Mage::getModel('customer/customer')->getCollection();
-
-foreach($customers as $customer){
-
-    $customerId = $customer->getId();
-
-    $currentCustomer = Mage::getModel('customer/customer')->load($customerId);
-
-    $websiteId = $currentCustomer->getWebsiteId();
-
-    $balanceModel = Mage::getModel('enterprise_customerbalance/balance')
-    	     ->setCustomerId($customerId)
-             ->setWebsiteId($websiteId)
-             ->loadByCustomer();
-
-    // check if balance found
-    if (!$balanceModel->getId()) {
-          $balance = (float) 0;
-    }else{
-
-          $balance = $balanceModel->getAmount();
-     }
-
-    $currentCustomer->setStorebalanceexportcsvx($balance);
-
-    //the original version of this answer was wrong; need to use the resource model.
-    $currentCustomer->getResource()->saveAttribute($currentCustomer,'storebalanceexportcsvx');
-
-    $currentCustomer->save();
-
-}
-
-
+Mage::helper('balanceexport/attribute')->synchStoreBalanceAttribute();
 
 
 $installer->endSetup();
